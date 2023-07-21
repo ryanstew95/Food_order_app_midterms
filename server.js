edbd50ef2bc929899461e06d79eaacd66860a702
Ryan Stewart <ryanstew17@gmail.com>
2023-07-20 22:19:53 -0300

    working on login setup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 // Web server config
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
+const session = require('express-session');
 const morgan = require('morgan');
 
 const PORT = process.env.PORT || 8080;
@@ -25,6 +26,16 @@ app.use(
   })
 );
 app.use(express.static('public'));
+
+// Set up session middleware
+app.use(
+  session({
+    secret: 'tyh6v3rg',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
@@ -48,22 +59,54 @@ app.listen(PORT, () => {
   console.log(`food order app listening on port ${PORT}`);
 });
 
-// routes
+const mockUsers = [
+  { username: 'user1', password: 'password1' },
+  { username: 'user2', password: 'password2' },
+  // Add more users as needed
+];
+
+// Routes
 app.get('/login', (req, res) => {
-  res.render('index.ejs');
-  //login submit forumn
+  res.render('login');
 });
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Check the user credentials (this is just a mock example)
+  const mockUsers = [
+    { username: 'user1', password: 'password1' },
+    { username: 'user2', password: 'password2' },
+    // Add more users as needed
+  ];
+
+  const user = mockUsers.find((user) => user.username === username && user.password === password);
+
+  if (user) {
+    // Successful login, redirect to the main page
+    res.redirect('/main');
+  } else {
+    // Failed login, redirect back to the login page with an error message
+    res.render('login', { error: 'Invalid username or password' });
+  }
+});
+
 app.get('/login/:id', (req, res) => {
-  // using encrypted cookies
+  // using encrypted cookies (you need to set up session/cookie middleware)
   req.session.user_id = req.params.id;
 
-  // or using plain-text cookies
+  // or using plain-text cookies (you need to set up session/cookie middleware)
   res.cookie('user_id', req.params.id);
 
-  // send the user to main page
-  res.redirect('/users.ejs');
-//
+  // send the user to the main page (index.ejs)
+  res.redirect('index.ejs');
 });
+
+app.get('/main', (req,res) => {
+  // display food items
+  res.render('index');
+});
+
 app.get('/users', (req, res) => {
   res.render('/users.ejs');
   // items
@@ -73,24 +116,21 @@ app.post('/users', (req, res) => {
   res.render('/users.ejs');
 
 });
-// psuedo code
-//users page
-//logging in - get
-// -main page - get
-// creating order -
 
-// action between the user and employee
-
-//employees
-// login / shows order - get -- hard coded isEmployee
-
-
-
-// (Browse, Read, Edit, Add, Delete)
-// /                                              - login
-// B - Get /Users 	            – render page
-// R - Get /Users/:id 	        – load food information for selected food type
-// E - Post /Users/:id                  – edit food information(stretch)
-// A - Post/Users  	            – create order
-
+/**
+ Login Page:
+GET /login: Display the login page to the user.
+Main Page:
+GET /main: Display the main page of your food app, showing various food items or categories.
+Employees Page (for viewing incoming orders):
+GET /employees: Display incoming orders for workers to see.
+GET /employees/order/:id: Display details of a specific order with ID :id.
+About Page:
+GET /about: Display information about your food app or your restaurant.
+Cart Page (assuming this is the page where users can view their cart and place orders):
+GET /cart: Display the contents of the user's shopping cart.
+POST /cart/add: Handle the addition of items to the cart.
+POST /cart/remove/:id: Handle the removal of a specific item with ID :id from the cart.
+POST /cart/checkout: Handle the checkout process and payment.
+ */
 
